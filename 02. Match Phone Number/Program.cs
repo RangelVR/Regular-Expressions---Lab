ab09a5d5796f7f6f4d634c7b78421684c10f9d1f
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-18 00:31:45 +0200

    Add files via upload

---
+++ 02. Match Phone Number/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+string phoneNumberas = Console.ReadLine();
+
+string pattern = @"\+359(.|-)2\1\d{3}\1\d{4}\b";
+
+MatchCollection matches = Regex.Matches(phoneNumberas, pattern);
+
+Console.WriteLine(string.Join(", ", matches));
