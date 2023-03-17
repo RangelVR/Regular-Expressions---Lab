8c7d97b75f3e383829a029005dea0906ef290210
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-18 01:00:35 +0200

    Add files via upload

---
+++ 03. Match Dates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+string dates = Console.ReadLine();
+
+string pattern = @"\b(?<date>\d{2})(\.|-|/)(?<month>[A-Z]{1}[a-z]{2})\1(?<year>\d{4})";
+
+MatchCollection matches = Regex.Matches(dates, pattern);
+
+foreach(Match match in matches)
+{
+    Console.WriteLine($"Day: {match.Groups["date"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");
+}
