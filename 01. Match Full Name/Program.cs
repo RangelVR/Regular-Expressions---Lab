168aa490a8a17e24982a12a279b591287af38a5b
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-18 00:02:26 +0200

    Add files via upload

---
+++ 01. Match Full Name/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+
+string pattern = @"\b[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+\b";
+
+MatchCollection matches = Regex.Matches(text, pattern);
+
+foreach (Match match in matches)
+{
+    Console.Write(match + " ");
+}
